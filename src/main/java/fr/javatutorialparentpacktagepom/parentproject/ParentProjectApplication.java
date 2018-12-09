b0576a2e39157b394d0e209d54153a0edd3d1f07
amdouniali33 <amdouniali33@gmail.com>
2018-12-09 11:24:48 +0100

    initilizing project in git

---
+++ src/main/java/fr/javatutorialparentpacktagepom/parentproject/ParentProjectApplication.java
@@ -0,0 +1,12 @@
+package fr.javatutorialparentpacktagepom.parentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentProjectApplication.class, args);
+	}
+}
